77b2a1fdc74f02d98de37a72bc9a42c5dd2ffb88
Minhazul Ashim <ashim.ashim7470@gmail.com>
2024-04-22 14:00:57 +0600

    added a module for practice

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { User } from './types.user';
 
 @Controller('users')
 export class UsersController {
@@ -7,7 +16,26 @@ export class UsersController {
 
   @Get()
   findAll() {
-    console.log('hit');
     return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Post()
+  store(@Body() createUserInput: User) {
+    return this.usersService.store(createUserInput);
+  }
+
+  @Put()
+  update(@Body() updateUserInput: User) {
+    return this.usersService.update(updateUserInput);
+  }
+
+  @Delete(':id')
+  async destroy(@Param('id') id: string) {
+    return this.usersService.destroy(parseInt(id));
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from './types.user';
 
 @Injectable()
 export class UsersService {
@@ -11,6 +12,25 @@ export class UsersService {
 
   async findOne(id: number) {
     return this.prisma.prismaClient.user.findUnique({
+      where: { id },
+    });
+  }
+
+  async store(body: User) {
+    return this.prisma.prismaClient.user.create({ data: { ...body } });
+  }
+
+  async update(body: User) {
+    const { id, ...rest } = body;
+    return this.prisma.prismaClient.user.update({
+      where: { id: id },
+      data: { ...rest },
+    });
+  }
+
+  async destroy(id: number) {
+    console.log(id);
+    return this.prisma.prismaClient.user.delete({
       where: { id },
     });
   }
