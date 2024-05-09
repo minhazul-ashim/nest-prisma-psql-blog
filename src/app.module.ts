acef1bb9a83cbdf25bae70c9ac5084a6494f5d52
Minhazul Ashim <ashim.ashim7470@gmail.com>
2024-05-09 20:07:10 +0600

    some more commits

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { UsersModule } from './users/users.module';
     AuthModule,
     PrismaModule,
     ConfigModule.forRoot({ envFilePath: ['.env'], isGlobal: true }),
+    BlogModule,
   ],
 })
 export class AppModule {}
