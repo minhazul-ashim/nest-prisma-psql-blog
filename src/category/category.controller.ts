927d12f89598856d942e07d11f6b8f594fb232fe
Minhazul Ashim <ashim.ashim7470@gmail.com>
2024-05-21 22:18:45 +0600

    implemented auth guard to handle unauthorized requests in the category module

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -6,9 +6,11 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryDto } from './dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('category')
 export class CategoryController {
@@ -23,16 +25,19 @@ export class CategoryController {
     return this.categoryService.getSingle(id);
   }
 
+  @UseGuards(AuthGuard)
   @Post()
   store(@Body() body: CategoryDto) {
     return this.categoryService.store(body);
   }
 
+  @UseGuards(AuthGuard)
   @Put(':id')
   update(@Param('id') id: string, @Body() body: CategoryDto) {
     return this.categoryService.update(id, body);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   destroy(@Param('id') id: string) {
     return this.categoryService.destroy(id);
